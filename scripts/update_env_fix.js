e188217d7bcb2ceef06f080c712b2a33bf5d316b
Dharaneeswar <dharaneeswar07@gmail.com>
2025-12-24 10:56:13 +0200

    Css update

--- scripts/update_env_fix.js
+++ scripts/update_env_fix.js
@@ -1,16 +1,1 @@
-const fs = require('fs');
-const path = require('path');
 
-const envPath = path.join(__dirname, '../.env');
-const credentials = `
-CLOUDINARY_CLOUD_NAME=dpoq9cbk5
-CLOUDINARY_API_KEY=339818724684276
-CLOUDINARY_API_SECRET=vp-LVF3p2R0lo5JX6pK_LaUWI14
-`;
-
-try {
-    fs.appendFileSync(envPath, credentials);
-    console.log('Successfully appended credentials to .env');
-} catch (error) {
-    console.error('Error updating .env:', error);
-}
